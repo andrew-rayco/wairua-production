89c5638347a373c09e5bafa3a663ba3aa5d1886e
Andrew <andy@andycondon.com>
2017-09-25 17:28:32 +1300

    change port for express server

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log('Wairua running on ' + PORT)
+});
